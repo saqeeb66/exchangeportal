b54b371b54ac59bcb86349c047b47f65f35fef6e
unknown <syedsaqeeb66@gmail.com>
2024-12-07 13:29:20 +0530

    first commit

---
+++ candidateform/src/test/java/com/kmdc/candidateform/CandidateformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmdc.candidateform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
